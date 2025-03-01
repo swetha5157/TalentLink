1ed7b6ae4f9d4dff8ab6181952f577ef7704ef05
Swetha S <swetha.s2022cce@sece.ac.in>
2025-03-01 19:12:46 +0530

    adding validations in job type,id validation..reducing the job controller code

--- middleware/validationHandler.js
+++ middleware/validationHandler.js
@@ -50,35 +50,35 @@ export const validateIdParam = withValidationErrors([
     if (!isValidMongoId) throw new BadRequestError("invalid MongoDB id");
     const job = await Job.findById(value);
     if (!job) throw new NotFoundError(`no job with id ${value}`);
-    const isAdmin = req.user.role === "admin";
-    const isOwner = req.user.userId === job.createdBy.toString();
+    // const isAdmin = req.user.role === "admin";
+    // const isOwner = req.user.userId === job.createdBy.toString();
 
-    if (!isAdmin && !isOwner)
-      throw new UnauthorizedError("not authorized to access this route");
+    // if (!isAdmin && !isOwner)
+    //   throw new UnauthorizedError("not authorized to access this route");
   }),
 ]);
 
-export const validateRegisterInput = withValidationErrors([
-  body("name").notEmpty().withMessage("name is required"),
-  body("email")
-    .notEmpty()
-    .withMessage("email is required")
-    .isEmail()
-    .withMessage("invalid email format")
-    .custom(async (email) => {
-      const user = await User.findOne({ email });
-      if (user) {
-        throw new BadRequestError("email already exists");
-      }
-    }),
-  body("password")
-    .notEmpty()
-    .withMessage("password is required")
-    .isLength({ min: 8 })
-    .withMessage("password must be at least 8 characters long"),
-  body("location").notEmpty().withMessage("location is required"),
-  body("lastName").notEmpty().withMessage("last name is required"),
-]);
+// export const validateRegisterInput = withValidationErrors([
+//   body("name").notEmpty().withMessage("name is required"),
+//   body("email")
+//     .notEmpty()
+//     .withMessage("email is required")
+//     .isEmail()
+//     .withMessage("invalid email format")
+//     .custom(async (email) => {
+//       const user = await User.findOne({ email });
+//       if (user) {
+//         throw new BadRequestError("email already exists");
+//       }
+//     }),
+//   body("password")
+//     .notEmpty()
+//     .withMessage("password is required")
+//     .isLength({ min: 8 })
+//     .withMessage("password must be at least 8 characters long"),
+//   body("location").notEmpty().withMessage("location is required"),
+//   body("lastName").notEmpty().withMessage("last name is required"),
+// ]);
 
 // export const validateLoginInput = withValidationErrors([
 //   body("email")
